3ea8207cfa8a4fa9ff10e21cd89a4ce08a61fff5
Andrew Garcia <garcia.gtr@gmail.com>
2018-09-28 13:05:08 -0400

    update plots

--- plot4.py
+++ plot4.py
@@ -8,15 +8,20 @@ z=5*np.sin(x)
 
 
 'plotting'
+#make plots
 
 #plt figure is a good way to return and number (or name) separate figures
 plt.figure(1)
-#make plots
 plt.plot(x,y,'k')
 plt.plot(x,z,'b')
 
 plt.figure(5)
 plt.plot(x,z,'bo')
 
+plt.xlabel(r'$\phi$  /  $\infty$')
+plt.ylabel('${E^2}\div{c^4}$  /  $kg^2$  ')
+
 plt.figure('another plot')
 plt.plot(x,y,'black',marker='^')
+plt.xlabel('x')
+plt.ylabel('y')
